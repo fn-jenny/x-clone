07625626edd93d312a5b46e42173ea1c03f8efe5
fn-jenny <fano.jenny@outlook.com>
2024-01-14 12:46:15 +0300

    Basic auth system using supabase auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from "next/headers";
 import Image from 'next/image';
+import AuthButton from './auth-button';
 
 export default async function Home() {
   const supabase = createServerActionClient({ cookies });
-  const { data: tweets } = await supabase.from("tweets").select();
+  const { data: tweets } = await supabase.from("tweets").select();  
 
   return (
-    //  Print out the tweets
-    <pre>{JSON.stringify(tweets, null, 2)}</pre>
+    <>
+      <AuthButton/>       
+      <pre>{JSON.stringify(tweets, null, 2)}</pre>
+    </>
   )
 }
