5d4985ccca7dc7165313a53203bce9169ee036f0
fn-jenny <fano.jenny@outlook.com>
2024-01-27 21:04:04 +0300

    Display Login / logout based on the session

---
+++ src/app/auth-button-server.tsx
@@ -0,0 +1,15 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import AuthButtonClient from "./auth-button-client";
+
+// Creating this auth Button server for getting session state from the server
+// Then rendering the client component with the state from the server to allow clickable button
+// This is a wworkaround for getting the session available on first render on client side
+export default async function AuthButtonServer() {
+  const supabase = createServerComponentClient({cookies})
+  const { 
+    data : { session },
+  } = await supabase.auth.getSession();
+
+  return <AuthButtonClient session={session} />
+}
