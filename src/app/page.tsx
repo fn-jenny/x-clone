908b54dc4160105c22beaf9d7887740e593be927
fn-jenny <fano.jenny@outlook.com>
2024-01-27 10:13:48 +0300

    Refresh token if it is expired
    To Look, it seems when modifying max-ages of the cookies, it do not regenerate that cookies.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
+import {  createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from "next/headers";
 import Image from 'next/image';
 import AuthButton from './auth-button';
 
 export default async function Home() {
-  const supabase = createServerActionClient({ cookies });
+  const supabase = createServerComponentClient({ cookies });
   const { data: tweets } = await supabase.from("tweets").select();  
 
   return (
